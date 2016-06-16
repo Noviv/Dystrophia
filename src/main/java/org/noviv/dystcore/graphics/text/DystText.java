852e3d5601a2bccb5af28e75fa72b32186b79fed
Matthew <matthewwebb65@yahoo.com>
2016-06-15 20:18:26 -0500

    began initial attempt at STB text rendering

--- src/main/java/org/noviv/dystcore/graphics/text/DystText.java
+++ src/main/java/org/noviv/dystcore/graphics/text/DystText.java
@@ -1,18 +1,48 @@
 package org.noviv.dystcore.graphics.text;
 
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
 import org.joml.Vector2d;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.stb.STBTTBakedChar;
+import org.lwjgl.stb.STBTTAlignedQuad;
 import org.noviv.dystcore.graphics.DystObject;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.stb.STBTruetype.*;
+import static org.lwjgl.system.MemoryUtil.memAllocFloat;
+import static org.noviv.dystcore.accessories.utilities.DystIO.ioResourceToByteBuffer;
 
 public class DystText extends DystObject {
 
+    private static final int BITMAP_W = 512;
+    private static final int BITMAP_H = 512;
+
+    private String text;
     private Vector2d position;
 
-    public DystText(String text, int x, int y) {
+    private int id;
+    private STBTTBakedChar.Buffer chardata;
+
+    public DystText(String tex, int x, int y) {
+        text = tex;
         position = new Vector2d(x, y);
     }
 
     @Override
     public void init() {
+        chardata = STBTTBakedChar.mallocBuffer(96);
+
+        ByteBuffer ttf = ioResourceToByteBuffer("fonts\\FiraSans.ttf", 160 * 1024);
+        ByteBuffer bitmap = BufferUtils.createByteBuffer(BITMAP_W * BITMAP_H);
+        stbtt_BakeFontBitmap(ttf, 24, bitmap, BITMAP_W, BITMAP_H, 32, chardata);
+        
+        id = glGenTextures();
+        glBindTexture(GL_TEXTURE_2D, id);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, BITMAP_W, BITMAP_H, 0, GL_ALPHA, GL_UNSIGNED_BYTE, bitmap);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glBindTexture(GL_TEXTURE_2D, 0);
     }
 
     @Override
@@ -21,6 +51,37 @@ public class DystText extends DystObject {
 
     @Override
     public void render() {
+        FloatBuffer xb = memAllocFloat(1);
+        FloatBuffer yb = memAllocFloat(1);
+
+        STBTTAlignedQuad q = STBTTAlignedQuad.malloc();
+
+        glBegin(GL_QUADS);
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c == '\n') {
+                yb.put(0, yb.get(0) + 24);
+                xb.put(0, 0.0f);
+                continue;
+            } else if (c < 32 || 128 <= c) {
+                continue;
+            }
+
+            stbtt_GetBakedQuad(chardata, BITMAP_W, BITMAP_H, c - 32, xb, yb, q, 1);
+
+            glTexCoord2f(q.s0(), q.t0());
+            glVertex2f(q.x0(), q.y0());
+
+            glTexCoord2f(q.s1(), q.t0());
+            glVertex2f(q.x1(), q.y0());
+
+            glTexCoord2f(q.s1(), q.t1());
+            glVertex2f(q.x1(), q.y1());
+
+            glTexCoord2f(q.s0(), q.t1());
+            glVertex2f(q.x0(), q.y1());
+        }
+        glEnd();
     }
 
     @Override
--- src/main/java/org/noviv/dystrophia/Main.java
+++ src/main/java/org/noviv/dystrophia/Main.java
@@ -1,39 +1,41 @@
 package org.noviv.dystrophia;
 
-import org.joml.Vector3f;
 import org.noviv.dystcore.DystEngine;
-import org.noviv.dystcore.accessories.utilities.DystTimer;
+import org.noviv.dystcore.graphics.text.DystText;
 
 public class Main {
 
     public static void main(String[] args) {
         DystEngine engine = new DystEngine();
-        Vector3f color1 = new Vector3f(1, 1, 1);
-        Vector3f color2 = new Vector3f(0, 0, 0);
-
-        int i = 0;
-        for (int x = -5; x <= 5; x++) {
-            for (int z = -5; z <= 5; z++) {
-                Squar square = new Squar(i % 2 == 0 ? color1 : color2);
-                engine.addObject(square);
-                square.move(new Vector3f(x, -1, z));
-                i++;
-            }
-        }
-
-        DystTimer timer = new DystTimer();
-        timer.setTimeTrigger(0.001);
-
-        Squar square = new Squar(color2);
-        square.move(new Vector3f(0, 1, 0));
-        engine.addObject(square);
+//        Vector3f color1 = new Vector3f(1, 1, 1);
+//        Vector3f color2 = new Vector3f(0, 0, 0);
+//
+//        int i = 0;
+//        for (int x = -5; x <= 5; x++) {
+//            for (int z = -5; z <= 5; z++) {
+//                Squar square = new Squar(i % 2 == 0 ? color1 : color2);
+//                engine.addObject(square);
+//                square.move(new Vector3f(x, -1, z));
+//                i++;
+//            }
+//        }
+//
+//        DystTimer timer = new DystTimer();
+//        timer.setTimeTrigger(0.001);
+//
+//        Squar square = new Squar(color2);
+//        square.move(new Vector3f(0, 1, 0));
+//        engine.addObject(square);
+//        engine.run();
+//
+//        while (engine.isRunning()) {
+//            if (timer.isTriggered()) {
+//                square.rotateX(.1f);
+//                square.move(new Vector3f(0, (float) (0.005 * Math.cos(10 * timer.getTime())), 0));
+//            }
+//        }
+        
+        engine.addObject(new DystText("hello world", 100, 100));
         engine.run();
-
-        while (engine.isRunning()) {
-            if (timer.isTriggered()) {
-                square.rotateX(.1f);
-                square.move(new Vector3f(0, (float) (0.005 * Math.cos(10 * timer.getTime())), 0));
-            }
-        }
     }
 }
